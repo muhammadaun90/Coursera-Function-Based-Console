cc1fc0e21bc5579485c983f0f7e8373c20d23fb0
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-07 16:29:28 +0500

    Finalized script.js file
    Final program.

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function consoleStyler(color, background, fontSize, txt) {
 function celebrateStyler(reason) {
     var fontStyle = "color: tomato; font-size: 50px";
     if (reason == "birthday") {
-        console.log(`&cHappy birthday`, fontStyle);
+        console.log(`%cHappy birthday`, fontStyle);
     }
     else if (reason == "champions") {
-        console.log(`&cCongrats on the title!`, fontStyle);
+        console.log(`%cCongrats on the title!`, fontStyle);
     } else {
-        console.log(`&c${reason}`, fontStyle);
+        console.log(message,style);
     }
 }
 
@@ -26,7 +26,9 @@ celebrateStyler('birthday');
 
 // Task 4: Insert a congratulatory and custom message.
 function styleAndCelebrate(color, background, fontSize, txt, reason) {
-    consoleStyler(color, background, fontSize, txt);
-    celebrateStyler(reason);
+    if (typeof reason == "string"){
+        consoleStyler(color, background, fontSize, txt);
+        celebrateStyler(reason);
+    }
 }
 styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it!', 'champions');
