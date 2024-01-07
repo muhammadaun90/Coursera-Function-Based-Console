b217da73186a1789b8c41a33b38605f2b090333e
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-07 15:02:17 +0500

    Update script.js
    Task1 completed as per the instructions given in the program.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Task1: Build a function-based console log message generator
+function consoleStyler(color, background, fontSize, txt) {
+    var message = "%c" + txt;
+    var style = `color: ${color};`
+    style += `background: ${background};`
+    style += `font-size: ${fontSize};`
+    console.log(message, style);
+}
